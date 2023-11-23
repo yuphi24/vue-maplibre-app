fda672aab10b4c7dd94404b8bb7fdd86fab2ee64
n-o-tud <nikolas.ott@tu-dresden.de>
2023-11-23 15:34:39 +0100

    refactor: only use of bootstraps classes for styling

--- src/components/left-panel/settings-panel/BaseMapSettings.vue
+++ src/components/left-panel/settings-panel/BaseMapSettings.vue
@@ -23,47 +23,53 @@ function changeBaseLayer(oldBaseLayer, newBaseLayer) {
 
 <template>
   <div class="base-map-settings">
-    <div class="settings-content-title">
-      <h3>Base Map</h3>
-    </div>
-    <div class="settings-content-body">
-      <div
-        class="card"
-        style="width: 9rem"
-        v-for="baseMap in baseMaps"
-        :key="baseMap.id"
-        @click="changeBaseLayer(activeBaseLayer, baseMap.id)"
+    <p class="d-inline-flex gap-1">
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#baseMaps"
+        aria-expanded="false"
+        aria-controls="baseMaps"
       >
-        <img
-          class="card-img-top"
-          :src="baseMap.cardImage"
-          alt="Card image cap"
-        />
-        <div class="card-body">
-          <h5 class="card-title">{{ baseMap.title }}</h5>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrows-expand"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2M8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10"
+          />
+        </svg>
+        Base Maps
+      </button>
+    </p>
+    <div class="collapse" id="baseMaps">
+      <div class="maps-content d-flex">
+        >
+        <div
+          class="card"
+          style="width: 9rem"
+          role="button"
+          v-for="baseMap in baseMaps"
+          :key="baseMap.id"
+          @click="changeBaseLayer(activeBaseLayer, baseMap.id)"
+        >
+          <img
+            class="card-img-top"
+            :src="baseMap.cardImage"
+            alt="Card image cap"
+          />
+          <p class="text-center">{{ baseMap.title }}</p>
+          <!-- <h5 class="card-title">{{ baseMap.title }}</h5> -->
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-.base-map-settings {
-  display: block;
-  height: auto;
-  height: 100;
-  padding: 1rem;
-}
-
-.settings-content-body {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.card {
-  height: auto;
-  /* width: 10rem; */
-  padding: 2px;
-  cursor: pointer;
-}
-</style>
+<style></style>
